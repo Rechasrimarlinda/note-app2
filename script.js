fba7eb71bc621107af1e68b0e775a048fe5092dd
Recha sri marlinda <rechasrimarlinda@gmail.com>
2020-12-14 08:50:46 +0700

    membuat event dengan addEventListerner (click) beserta function

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ var container2 = document.getElementsByClassName("container2")[0];
 var container3 = document.getElementsByClassName("container3")[0];
 var checkIcon = document.getElementById("check-icon");
 var xIcon = document.getElementById("x-icon");
+
